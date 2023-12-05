a52be6f9d9aa421d222760f7304b7412232219b6
din-s <sdinesh960124@gmail.com>
2023-12-05 18:12:10 +0530

    -- input , output decorators

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Product } from '../products';
 
 @Component({
   selector: 'app-product-alerts',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './product-alerts.component.html',
   styleUrl: './product-alerts.component.css'
 })
 export class ProductAlertsComponent {
-
+  @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
 }
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -13,6 +13,11 @@ export class ProductListComponent {
   share() {
     window.alert('The product has been shared!');
   }
+
+  public onNotify(event:any) {
+    window.alert("Notified");
+    console.log("notified")
+  }
 }
 
 
