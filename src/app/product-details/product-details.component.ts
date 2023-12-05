e0fea1148921b30a2a9ca10fa0d03861c7ac189e
din-s <sdinesh960124@gmail.com>
2023-12-05 18:43:59 +0530

    -- routing

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { Product, products } from '../products';
+
+@Component({
+  selector: 'app-product-details',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-details.component.html',
+  styleUrl: './product-details.component.css'
+})
+export class ProductDetailsComponent implements OnInit{
+
+  product: Product | undefined;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const routeMap = this.route.snapshot.paramMap;
+    const productId = Number(routeMap.get('productId'));
+
+    this.product = products.find((product) => product.id == productId);
+  }
+
+}
